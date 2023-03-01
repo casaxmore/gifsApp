a8f3af454395f3a3ca29b6222578f45ab3d80dcf
casaxmore <casaxmore@gmail.com>
2023-03-01 21:02:52 +0100

    paginación

--- src/app/gifs/pipes/filtro.pipe.ts
+++ src/app/gifs/pipes/filtro.pipe.ts
@@ -8,11 +8,11 @@ export class FiltroPipe implements PipeTransform {
   transform(resultados: any, page: number = 0, search:string = ''): any {
 
     if(search.length === 0){
-      return resultados.slice(page, page + 5);
+      return resultados.slice(page, page + 6);
     }
 
     const filteredGifs = resultados.filter( (gif: any) => gif.includes(search));
-    return filteredGifs.slice(page, page + 5);
+    return filteredGifs.slice(page, page + 6);
 
   }
 
